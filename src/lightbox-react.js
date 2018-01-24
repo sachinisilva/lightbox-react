18017e5f91e7f0e405bfcba549e8a23a16f6099f
Silva <USILVSA@PEROOT.COM>
2018-01-24 12:24:21 +0530

    Added stopPropagation

--- src/lightbox-react.js
+++ src/lightbox-react.js
@@ -793,7 +793,7 @@ class LightboxReact extends Component {
 
     handleTouchStart(event) {
         event.stopPropagation()
-        if (this.shouldHandleEvent(SOURCE_TOUCH)) {
+        if (this.shouldHandleEvent(SOURCE_TOUCH) && LightboxReact.isTargetMatchImage(event.target)) {
             [].forEach.call(event.changedTouches,
                 eventTouch => this.addPointer(LightboxReact.parseTouchPointer(eventTouch)));
             this.multiPointerStart(event);
