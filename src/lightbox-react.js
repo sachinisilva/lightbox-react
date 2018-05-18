f49f14cdedf2eaf5762e3bb118250dda8c8f3ec3
Silva <USILVSA@PEROOT.COM>
2018-05-18 16:07:42 +0530

    Added role dialog

--- src/lightbox-react.js
+++ src/lightbox-react.js
@@ -1489,7 +1489,8 @@ class LightboxReact extends Component {
                 onRequestClose={clickOutsideToClose ? this.requestClose : noop}
                 onAfterOpen={() => this.outerEl && this.outerEl.focus()} // Focus on the div with key handlers
                 style={modalStyle}
-                contentLabel={translate('Lightbox')}
+                role='dialog'
+                contentLabel='Lightbox'
             >
                 <div // eslint-disable-line jsx-a11y/no-static-element-interactions
                     // Floating modal with closing animations
